e1cd27f5d9be501293f1e6cdf46032bb1df45afa
houfachao <houfachao@58.com>
2021-11-25 23:11:58 +0800

    剑指 Offer 53 - II. 0～n-1中缺失的数字

---
+++ src/main/java/com/geekidentity/leetcode/offer/search/Offer35IIMissingNumber.java
@@ -0,0 +1,30 @@
+package com.geekidentity.leetcode.offer.search;
+
+/**
+ * 查找算法
+ * 剑指 Offer 53 - II. 0～n-1中缺失的数字
+ * https://leetcode-cn.com/problems/que-shi-de-shu-zi-lcof/
+ */
+public class Offer35IIMissingNumber {
+    public int missingNumber(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int left = 0, right = nums.length - 1, middle = 0;
+        while (left <= right) {
+            middle = (left + right) / 2;
+            if (nums[middle] != middle) {
+                if (middle == 0 || nums[middle - 1] == middle - 1) {
+                    return middle;
+                }
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+        if (left == nums.length) {
+            return nums.length;
+        }
+        return middle;
+    }
+}
