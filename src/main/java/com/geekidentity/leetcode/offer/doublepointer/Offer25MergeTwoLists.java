49c580dc3ba4b7df1a8b8b8c8da62665ef49419a
houfachao <houfachao@58.com>
2021-11-28 23:07:58 +0800

    剑指 Offer 25. 合并两个排序的链表

---
+++ src/main/java/com/geekidentity/leetcode/offer/doublepointer/Offer25MergeTwoLists.java
@@ -0,0 +1,40 @@
+package com.geekidentity.leetcode.offer.doublepointer;
+
+/**
+ * 双指针
+ * 剑指 Offer 25. 合并两个排序的链表
+ * https://leetcode-cn.com/problems/he-bing-liang-ge-pai-xu-de-lian-biao-lcof/
+ */
+public class Offer25MergeTwoLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        }
+
+        ListNode pre = new ListNode(0), cur = pre;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        if (l1 != null) {
+            cur.next = l1;
+        }
+        if (l2 != null) {
+            cur.next = l2;
+        }
+        return pre.next;
+    }
+    public static class ListNode {
+       int val;
+       ListNode next;
+       ListNode(int x) { val = x; }
+   }
+}
