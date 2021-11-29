82b78aa0fbcf712b980abb3f13d0726ec3873ce6
houfachao <houfachao@58.com>
2021-11-29 22:35:05 +0800

    剑指 Offer 21. 调整数组顺序使奇数位于偶数前面

---
+++ src/main/java/com/geekidentity/leetcode/offer/doublepointer/Offer21Exchange.java
@@ -0,0 +1,30 @@
+package com.geekidentity.leetcode.offer.doublepointer;
+
+/**
+ * 双指针
+ * 剑指 Offer 21. 调整数组顺序使奇数位于偶数前面
+ * https://leetcode-cn.com/problems/diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof/
+ */
+public class Offer21Exchange {
+    public int[] exchange(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return nums;
+        }
+        int length = nums.length;
+        int left = 0, right = length - 1;
+        while (left < right) {
+            while (left < length && nums[left] % 2 != 0) {
+                left++;
+            }
+            while (right >= 0 && nums[right] % 2 == 0) {
+                right--;
+            }
+            if (left < right) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+            }
+        }
+        return nums;
+    }
+}
