9efc142e83cb256b9760635d4c69858fe2129d5d
houfachao <houfachao@58.com>
2021-12-01 09:19:05 +0800

    剑指 Offer 27. 二叉树的镜像

--- src/main/java/com/geekidentity/leetcode/offer/backtracking/Offer27MirrorTree.java
+++ src/main/java/com/geekidentity/leetcode/offer/backtracking/Offer27MirrorTree.java
@@ -6,4 +6,25 @@ package com.geekidentity.leetcode.offer.backtracking;
  * https://leetcode-cn.com/problems/er-cha-shu-de-jing-xiang-lcof/
  */
 public class Offer27MirrorTree {
+    public TreeNode mirrorTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        if (root.left == null && root.right == null) {
+            return root;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        mirrorTree(root.left);
+        mirrorTree(root.right);
+        return root;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
 }
