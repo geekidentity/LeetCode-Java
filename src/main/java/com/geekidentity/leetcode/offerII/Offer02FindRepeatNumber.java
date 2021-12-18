de28e3ae88733ba36fd32bf2137a8465d56606f3
houfachao <geekidentity@163.com>
2021-12-18 12:49:34 +0800

    剑指 Offer 03. 数组中重复的数字

---
+++ src/main/java/com/geekidentity/leetcode/offerII/Offer02FindRepeatNumber.java
@@ -0,0 +1,30 @@
+package com.geekidentity.leetcode.offerII;
+
+/**
+ * 查找算法
+ * 剑指 Offer 03. 数组中重复的数字
+ * https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/
+ */
+public class Offer02FindRepeatNumber {
+
+    public int findRepeatNumber(int[] nums) {
+        if (nums == null) {
+            return -1;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] != i) {
+                if (nums[i] == nums[nums[i]]) {
+                    return nums[i];
+                }
+                swap(nums, i, nums[i]);
+            }
+        }
+        return -1;
+    }
+
+    private void swap(int[] nums, int a, int b) {
+        int t = nums[a];
+        nums[a] = nums[b];
+        nums[b] = t;
+    }
+}
