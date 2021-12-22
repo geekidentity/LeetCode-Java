347e7125af20df4d516ecbbd85614ebcee22a8bd
houfachao <geekidentity@163.com>
2021-12-22 23:57:48 +0800

    动态规划

--- src/main/java/com/geekidentity/leetcode/offerII/Offer14CuttingRope.java
+++ src/main/java/com/geekidentity/leetcode/offerII/Offer14CuttingRope.java
@@ -13,4 +13,31 @@ public class Offer14CuttingRope {
         if(b == 1) return (int)Math.pow(3, a - 1) * 4;
         return (int)Math.pow(3, a) * 2;
     }
+
+    /**
+     * 使用动态规划更好理解
+     */
+    public int dp(int n) {
+        if (n < 2) return 0;
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        int[] product = new int[n + 1];
+
+        product[0] = 0;
+        product[1] = 1;
+        product[2] = 2;
+        product[3] = 3;
+
+        int max = 0;
+        for (int i = 4; i <= n; i++) {
+            max = 0;
+            // i/2 是为了减少无用匹配
+            for (int j = 1; j <= i / 2; j++) {
+                int p = product[j] * product[i - j];
+                if (max < p) max = p;
+                product[i] = max;
+            }
+        }
+        return product[n];
+    }
 }
