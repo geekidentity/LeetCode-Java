680e82d0169488f98e97e9ab512420f702fc435f
houfachao <geekidentity@163.com>
2021-12-23 00:06:25 +0800

    剑指 Offer 15. 二进制中1的个数

---
+++ src/main/java/com/geekidentity/leetcode/offerII/Offer15HammingWeight.java
@@ -0,0 +1,17 @@
+package com.geekidentity.leetcode.offerII;
+
+/**
+ * 位运算
+ * 剑指 Offer 15. 二进制中1的个数
+ * https://leetcode-cn.com/problems/er-jin-zhi-zhong-1de-ge-shu-lcof/
+ */
+public class Offer15HammingWeight {
+    public int hammingWeight1(int n) {
+        int result = 0;
+        while (n != 0) {
+            result += n & 1;
+            n >>>= 1;
+        }
+        return result;
+    }
+}
