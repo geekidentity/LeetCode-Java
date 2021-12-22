2461ed12dca242ab914e195152c7ca5d007ef103
houfachao <geekidentity@163.com>
2021-12-23 00:15:23 +0800

    Java中 >> 表示有符号右移，>>> 为无符号右移

--- src/main/java/com/geekidentity/leetcode/offerII/Offer15HammingWeight.java
+++ src/main/java/com/geekidentity/leetcode/offerII/Offer15HammingWeight.java
@@ -4,6 +4,7 @@ package com.geekidentity.leetcode.offerII;
  * 位运算
  * 剑指 Offer 15. 二进制中1的个数
  * https://leetcode-cn.com/problems/er-jin-zhi-zhong-1de-ge-shu-lcof/
+ * Java中 >> 表示有符号右移，>>> 为无符号右移
  */
 public class Offer15HammingWeight {
     public int hammingWeight1(int n) {
