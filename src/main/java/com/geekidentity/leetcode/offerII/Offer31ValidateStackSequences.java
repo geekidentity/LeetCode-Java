91f5107180bb1a29a393884ff2d029152c0e0e34
houfachao <geekidentity@163.com>
2022-01-10 23:06:14 +0800

    说明

--- src/main/java/com/geekidentity/leetcode/offerII/Offer31ValidateStackSequences.java
+++ src/main/java/com/geekidentity/leetcode/offerII/Offer31ValidateStackSequences.java
@@ -6,6 +6,10 @@ import java.util.Stack;
  * 模拟
  * 剑指 Offer 31. 栈的压入、弹出序列
  * https://leetcode-cn.com/problems/zhan-de-ya-ru-dan-chu-xu-lie-lcof/
+ *
+ * 1. 如果下一个弹出的数字刚好是栈顶数字，那么直接弹出；
+ * 2. 如果下一个弹出的数字不在栈顶，则把压栈序列中还没有入栈的数字压入辅助栈，直到把下一个需要弹出的数字压入栈顶为止。
+ * 3. 如果所有数字都压入栈，还没有下一个弹出的数字（stack不为空），则该序列不是一个弹出序列。
  */
 public class Offer31ValidateStackSequences {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
