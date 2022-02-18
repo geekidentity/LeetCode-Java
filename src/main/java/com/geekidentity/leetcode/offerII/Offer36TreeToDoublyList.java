35ec1f6a70d53329cc804a4ad0739d9b3356cfe0
houfachao <geekidentity@163.com>
2022-02-18 09:17:33 +0800

    剑指 Offer 36. 二叉搜索树与双向链表

--- src/main/java/com/geekidentity/leetcode/offerII/Offer36TreeToDoublyList.java
+++ src/main/java/com/geekidentity/leetcode/offerII/Offer36TreeToDoublyList.java
@@ -18,6 +18,11 @@ public class Offer36TreeToDoublyList {
         return head;
     }
 
+    /**
+     * 当 pre 为 null 时，代表正在访问 链表头结点，记为 head。
+     * 当 pre 不为 null 时，修改双向链表引用，即： pre.right = cur; cur.left = pre。
+     * 保存 cur：更新 pre = cur，即节点 cur 是后继节点的 pre；
+     */
     private void dfs(Node cur) {
         if (cur == null) {
             return;
