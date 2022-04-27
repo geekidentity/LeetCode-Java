f93be64ed46d3095b100fe94c4ad054935fef41e
houfachao <geekidentity@163.com>
2022-04-27 23:30:34 +0800

    swap int array

---
+++ src/main/java/com/geekidentity/leetcode/utils/ArrayUtils.java
@@ -0,0 +1,10 @@
+package com.geekidentity.leetcode.utils;
+
+public class ArrayUtils {
+
+    public static void swap(int[] array, int a, int b) {
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+}
