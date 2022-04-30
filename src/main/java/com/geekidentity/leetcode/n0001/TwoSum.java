2f74607f61d94153ac2a7ff22d2a590bc0ace84b
houfachao <geekidentity@163.com>
2022-04-30 20:08:43 +0800

    1. 两数之和

---
+++ src/main/java/com/geekidentity/leetcode/n0001/TwoSum.java
@@ -0,0 +1,33 @@
+package com.geekidentity.leetcode.n0001;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1. 两数之和
+ * https://leetcode-cn.com/problems/two-sum/
+ */
+public class TwoSum {
+
+    public int[] twoSum1(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) return new int[]{i, j};
+            }
+        }
+        return new int[0];
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(nums.length);
+
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff)) {
+                return new int[]{i, map.get(diff)};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
